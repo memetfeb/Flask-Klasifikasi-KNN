a9d29145bc50a4351ef6c9d07f6a6118cb1042c2
Memet Febryanto <42493473+memetfeb@users.noreply.github.com>
2021-07-28 20:21:12 +0700

    update tampilan result

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, send_file
 from flask import request
 import pandas as pd
 import numpy as np
@@ -13,143 +13,104 @@ from imblearn.over_sampling import SMOTE
 import pengujian as p1
 import time
 from flask_paginate import Pagination, get_page_args, get_page_parameter
+# save numpy array as csv file
+from numpy import asarray
+from numpy import savetxt
 
 app = Flask(__name__)
 
-# Fungsi untuk mendapatkan nilai min dan max
-def getMinMax(x):
-  minmax = []
-  for i in range(0,21):
-    kolom =[]
-    min = x[0][i]
-    max = x[0][i]
-    for j in range(len(x)):
-      if min > x[j][i]:
-        min = x[j][i]
-      if max < x[j][i]:
-        max = x[j][i]
-    kolom.append(min)
-    kolom.append(max)
-    minmax.append(kolom)
-  return minmax
-
-# Fungsi Manual MinMax Scaler
-def MinMaxScalerManual(fit,b):
-  minmax = getMinMax(fit)
-  hasil = []
-  for i in range(len(b)):
-    baris = []
-    for j in  range(len(b[i])):
-      pembagi = minmax[j][1]-minmax[j][0]
-      if (pembagi == 0):
-        pembagi = 1
-      c = (b[i][j] - minmax[j][0])/pembagi
-      baris.append(c)
-    hasil.append(baris)
-  return hasil
-
-
-
-# Fungsi Prediksi KNN
-def predictknn(point, data, label, n):
-  distances = []
-  for i in range(len(data)):
-    distances.append([p1.euclidean(point, data[i]), label[i]])
-  distances.sort(key=lambda x: x[0])
-  top_classes = [x[1] for x in distances[0:n]]
-  repeat = True
-  try:
-    y_hat = mode(top_classes)
-  except:
-    while repeat:
-      try:
-        top_classes = top_classes[:-1]
-        y_hat = mode(top_classes)
-        repeat = False
-      except:
-        pass
-  return y_hat
-
-# Fungsi KNN Manual
-def diagnosis(a):
-    #Import from csv
-    d = pd.read_csv('static/fetal_health.csv')
-    X = d.drop(columns="fetal_health")
-    y = d.fetal_health
-    y = np.array(y)
-
-    # Normalisasi Data
-    X_scaled = MinMaxScalerManual(np.array(X),np.array(X))
-
-    # Balancing DATA SMOTE
-    oversample = SMOTE()
-    X_scaled, y = oversample.fit_resample(X_scaled, y)
-
-    # Data Test
-    data = [120.0,0.0,0.0,0.0,0.0,0.0,0.0,73.0,0.5,43.0,2.4,64.0,62.0,126.0,2.0,0.0,120.0,137.0,121.0,73.0,1.0]
-    data = np.array(a).reshape(1, 21)
-    data = MinMaxScalerManual(np.array(X),data)
-
-    # Melakukan Prediksi dengan KNN
-    prediksi = predictknn(data, X_scaled, y, 4)
-    
-    # Mengolah hasil Prediksi
-    hasil = ""
-    if(prediksi == 1):
-        hasil = "Normal"
-    elif(prediksi == 2):
-        hasil = "Suspect"
-    else:
-        hasil = "Pathological"
-
-    return hasil
+#FORM KLASIFIKASI
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+  return render_template('index.html')
 
-@app.route("/index2")
-def index2():
-    return render_template('index2.html')
+#END FORM KLASIFIKASI
+
+#GET DATA LATIH TERBAIK
+
+@app.route("/data_latih")
+def data_latih():
+
+  #Import from csv
+  d = pd.read_csv('static/fetal_health.csv')
+  X = d.drop(columns="fetal_health")
+  y = d.fetal_health
+  y = np.array(y)
+
+  # Normalisasi Data
+  X_scaled = p1.MinMaxScalerManual(np.array(X),np.array(X))
+
+  # Balancing DATA SMOTE
+  oversample = SMOTE()
+  X_scaled, y = oversample.fit_resample(X_scaled, y)
+
+  # Join Data dan Label
+  y1 = y.reshape(4965, 1) 
+  data = np.hstack((y1, X_scaled))
+
+  kelas1 = p1.label1(data)
+  kelas2 = p1.label2(data)
+  kelas3 = p1.label3(data)
+
+  data1 = p1.getDataLatih(kelas1, kelas2, kelas3, 5)
+  data_latih1 = np.array(data1[0]).round(4)
+  # csv = '1,2,3\n4,5,6\n'
+  download = np.savetxt("data_latih.csv", data_latih1, delimiter=",")
+
+  download
+  return render_template('index.html')
+  #return Response(data_latih1,mimetype="text/csv",headers={"Content-disposition":"attachment; filename=data_latih.csv"})
+  # return np.savetxt("data_latih.csv", data_latih1, delimiter=",")
+  #return savetxt('data.csv', data_latih1, delimiter=',')
+  #return send_file(data_latih1,mimetype="text/csv",attachment_filename="export.csv",
+
+#END GET DATA LATIH TERBAIK
+
+#PROSES KLASIFIKASI DATA BARU
 
 @app.route("/result", methods=["POST"])
 def result():
     
-    # Start Time
-    start_time = time.time()
+  # Start Time
+  start_time = time.time()
     
-    data = []
-    data.append(float(request.form.get("baseline_value"))) #data0
-    data.append(float(request.form.get("accelerations"))) #data1
-    data.append(float(request.form.get("fetal_movement"))) #data2
-    data.append(float(request.form.get("uterine_contractions"))) #data3
-    data.append(float(request.form.get("light_decelerations"))) #data4
-    data.append(float(request.form.get("severe_decelerations"))) #data5
-    data.append(float(request.form.get("prolongued_decelerations"))) #data6
-    data.append(float(request.form.get("abnormal_short_term_variability"))) #data7
-    data.append(float(request.form.get("mean_value_of_short_term_variability"))) #data8
-    data.append(float(request.form.get("percentage_of_time_with_abnormal"))) #data9
-    data.append(float(request.form.get("mean_value_of_long_term_variability"))) #data10
-    data.append(float(request.form.get("histogram_width"))) #data11
-    data.append(float(request.form.get("histogram_min"))) #data12
-    data.append(float(request.form.get("histogram_max"))) #data13
-    data.append(float(request.form.get("histogram_number_of_peaks"))) #data14
-    data.append(float(request.form.get("histogram_number_of_zeroes"))) #data15
-    data.append(float(request.form.get("histogram_mode"))) #data16
-    data.append(float(request.form.get("histogram_mean"))) #data17
-    data.append(float(request.form.get("histogram_median"))) #data18
-    data.append(float(request.form.get("histogram_variance"))) #data19
-    data.append(float(request.form.get("histogram_tendency"))) #data20
+  data = []
+  data.append(float(request.form.get("baseline_value"))) #data0
+  data.append(float(request.form.get("accelerations"))) #data1
+  data.append(float(request.form.get("fetal_movement"))) #data2
+  data.append(float(request.form.get("uterine_contractions"))) #data3
+  data.append(float(request.form.get("light_decelerations"))) #data4
+  data.append(float(request.form.get("severe_decelerations"))) #data5
+  data.append(float(request.form.get("prolongued_decelerations"))) #data6
+  data.append(float(request.form.get("abnormal_short_term_variability"))) #data7
+  data.append(float(request.form.get("mean_value_of_short_term_variability"))) #data8
+  data.append(float(request.form.get("percentage_of_time_with_abnormal"))) #data9
+  data.append(float(request.form.get("mean_value_of_long_term_variability"))) #data10
+  data.append(float(request.form.get("histogram_width"))) #data11
+  data.append(float(request.form.get("histogram_min"))) #data12
+  data.append(float(request.form.get("histogram_max"))) #data13
+  data.append(float(request.form.get("histogram_number_of_peaks"))) #data14
+  data.append(float(request.form.get("histogram_number_of_zeroes"))) #data15
+  data.append(float(request.form.get("histogram_mode"))) #data16
+  data.append(float(request.form.get("histogram_mean"))) #data17
+  data.append(float(request.form.get("histogram_median"))) #data18
+  data.append(float(request.form.get("histogram_variance"))) #data19
+  data.append(float(request.form.get("histogram_tendency"))) #data20
     
-    print(f"data: {data} ")
-    hasil = diagnosis(data)
+  print(f"data: {data} ")
+  hasil = p1.diagnosis(data)
 
-    # End Time
-    end_time = time.time()
-    time_result = end_time - start_time
-    time_result = p1.time_convert(time_result)
+  # End Time
+  end_time = time.time()
+  time_result = end_time - start_time
+  time_result = p1.time_convert(time_result)
 
-    return render_template('result.html', data=data, hasil=hasil, time_result=time_result)
+  return render_template('result.html', data=data, hasil=hasil, time_result=time_result)
+
+#END PROSES KLASIFIKASI DATA BARU
+
+#PROSES PENGUJIAN
 
 @app.route("/pengujian")
 def pengujian():
@@ -164,7 +125,7 @@ def pengujian():
   y = np.array(y)
 
   # Normalisasi Data
-  X_scaled = MinMaxScalerManual(np.array(X),np.array(X))
+  X_scaled = p1.MinMaxScalerManual(np.array(X),np.array(X))
 
   # Balancing DATA SMOTE
   oversample = SMOTE()
@@ -181,7 +142,7 @@ def pengujian():
   knn = p1.pengujian1(kelas1,kelas2,kelas3)
   lenknn = len(knn[1])
 
-  kfold = p1.pengujian2(kelas1,kelas2,kelas3)
+  kfold = p1.pengujian2(kelas1,kelas2,kelas3,10,4)
   lenkfold = len(kfold[1])
 
   end_time_pengujian = time.time()
@@ -190,27 +151,51 @@ def pengujian():
   
   return render_template('pengujian.html', knn=knn, lenknn=lenknn, kfold=kfold, lenkfold=lenkfold ,time_pengujian=time_pengujian)
 
-#Import data asli / data awal from csv
-data_asli = np.array(pd.read_csv('static/fetal_health.csv'))
+#END PROSES PENGUJIAN
 
+#PROSES DATA AWAL
+
+data_asli = np.array(pd.read_csv('static/fetal_health.csv'))
 def get_users_data_asli(offset=0, per_page=100):
     return data_asli[offset: offset + per_page]
-
 @app.route("/datasetasli")
 def datasetasli():
-
-   #Import from csv
-  # d = pd.read_csv('static/fetal_health.csv')
-  # X = d.drop(columns="fetal_health")
-  # y = d.fetal_health
-  # y = np.array(y)
-  # data = np.array(d)
-
   page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
   total = len(data_asli)
   pagination_users = get_users_data_asli(offset=offset, per_page=per_page)
   pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
   return render_template('datasetasli.html', users=pagination_users, page=page, per_page=per_page, pagination=pagination,)
+
+#END PROSES DATA AWAL
+
+# PROSES SMOTE
+
+d_smote = pd.read_csv('static/fetal_health.csv')
+X_asli = d_smote.drop(columns="fetal_health")
+y_asli = np.array(d_smote.fetal_health)
+oversample = SMOTE()
+X_smote, y_smote = oversample.fit_resample(np.array(X_asli), y_asli)
+y_smote = y_smote.reshape(4965, 1) 
+data_smote = np.hstack((X_smote, y_smote))
+data_smote = np.round(data_smote, 3)
+def get_users_data_smote(offset=0, per_page=100):
+    return data_smote[offset: offset + per_page]
+@app.route("/datasetsmote")
+def datasetsmote():
+  page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+  total = len(data_smote)
+  pagination_users = get_users_data_smote(offset=offset, per_page=per_page)
+  pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
+  return render_template('datasetsmote.html', users=pagination_users, page=page, per_page=per_page, pagination=pagination,)
+
+#END PROSES SMOTE
+
+
+# BEKAS BEKAS
+
+@app.route("/index2")
+def index2():
+  return render_template('index2.html')
 
 @app.route("/datasetasli2")
 def datasetasli2():
@@ -225,46 +210,6 @@ def datasetasli2():
 
   lendata = len(data) 
   return render_template('datasetasli2.html', data=data, lendata=lendata)
-
-
-#Data SMOTE
-# dataasli = np.array(pd.read_csv('static/fetal_health.csv'))
-d_smote = pd.read_csv('static/fetal_health.csv')
-X_asli = d_smote.drop(columns="fetal_health")
-y_asli = np.array(d_smote.fetal_health)
-
-#SMOTE
-oversample = SMOTE()
-X_smote, y_smote = oversample.fit_resample(np.array(X_asli), y_asli)
-
-# Join Data dan Label
-y_smote = y_smote.reshape(4965, 1) 
-data_smote = np.hstack((X_smote, y_smote))
-data_smote = np.round(data_smote, 3)
-
-def get_users_data_smote(offset=0, per_page=100):
-    return data_smote[offset: offset + per_page]
-@app.route("/datasetsmote")
-def datasetsmote():
-
-  #Import from csv
-  # d = pd.read_csv('static/fetal_health.csv')
-  # X = d.drop(columns="fetal_health")
-  # y = d.fetal_health
-  # y = np.array(y)
-
-  # oversample = SMOTE()
-  # X_scaled, y = oversample.fit_resample(np.array(X), y)
-
-  # Join Data dan Label
-  # y1 = y.reshape(4965, 1) 
-  # data = np.hstack((X_scaled, y1))
-
-  page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-  total = len(data_smote)
-  pagination_users = get_users_data_smote(offset=offset, per_page=per_page)
-  pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
-  return render_template('datasetsmote.html', users=pagination_users, page=page, per_page=per_page, pagination=pagination,)
 
 @app.route("/datasetsmote1")
 def datasetsmote1():
@@ -283,15 +228,7 @@ def datasetsmote1():
   data = np.hstack((X_scaled, y1))
 
   lendata = len(data) 
-  return render_template('datasetsmote.html', data=data, lendata=lendata)
-
-
-# DATA AWAL
-
-
-# def get_users(offset=0, per_page=100):
-#     return data[offset: offset + per_page]
-
+  return render_template('datasetsmote1.html', data=data, lendata=lendata)
 
 
 if __name__ == "__main__":
--- pengujian.py
+++ pengujian.py
@@ -3,7 +3,22 @@ import numpy as np
 from statistics import mode
 from math import sqrt
 import time
+import scipy
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+import imblearn
+from imblearn.over_sampling import SMOTE
 
+
+# Variabel Global
+
+dataset = pd.read_csv('static/fetal_health.csv')
+X_dataset = dataset.drop(columns="fetal_health")
+X_dataset = np.array(X_dataset)
+y_dataset = dataset.fetal_health
+y_dataset = np.array(y_dataset)
+
+#TIMER
 def time_convert(sec):
   mins = sec // 60
   sec = round(sec % 60, 1)
@@ -17,9 +32,16 @@ def time_convert(sec):
   else:
     return "{0} Jam {1} Menit {2} Detik".format(int(hours),int(mins),sec)
 
-# Fungsi Euclidean
+#END TIMER
+
+#EUCLIDEAN
+
 def euclidean(r1, r2):
   return np.sqrt(np.sum((np.subtract(r1,r2))**2))
+
+#END EUCLIDEAN
+
+#KNN
 
 def knn(X_train, y_train, X_test, y_test, n):
   y_hat = [classify(point, X_train, y_train,n) for point in X_test]
@@ -34,7 +56,6 @@ def classify(point, data, label, n):
     distances.append([euclidean(point, data[i]), label[i]])
   distances.sort(key=lambda x: x[0])
   top_classes = [x[1] for x in distances[0:n]]
-  # print(f"top_classes: { top_classes } ")
   repeat = True
   try:
     y_hat = mode(top_classes)
@@ -46,9 +67,127 @@ def classify(point, data, label, n):
         repeat = False
       except:
         pass
-        
   return y_hat
 
+#END KNN
+
+# DIAGNOSIS
+
+def diagnosis(a):
+  y = []
+  X = []
+  #Import from csv
+  d = pd.read_csv('static/data_latih_terbaik.csv', header=None)
+  # X = d.drop(columns="fetal_health")
+  # y = d.fetal_health
+  # y = np.array(y)
+  d = np.array(d)
+  for j in range(len(d)):
+    # print(f"data Train: {j}; ")
+    y.append(d[j][0])
+    X = np.delete(d, [0,0], axis=1)
+  
+  # Normalisasi Data
+  # X_scaled = MinMaxScalerManual(np.array(X),np.array(X))
+
+  # Balancing DATA SMOTE
+  # oversample = SMOTE()
+  # X_scaled, y = oversample.fit_resample(X_scaled, y)
+
+  # Join Data dan Label
+  # y1 = y.reshape(4965, 1) 
+  # dataset = np.hstack((y1, X_scaled))
+
+  # kelas1 = label1(dataset)
+  # kelas2 = label2(dataset)
+  # kelas3 = label3(dataset)
+
+  # kelas_data_latih = []
+
+  # data_latih = getDataLatih(kelas1, kelas2, kelas3, 5)
+  data_uji = np.array(a).reshape(1, 21)
+  data_uji = MinMaxScalerManual(X_dataset,data_uji)
+
+  # Melakukan Prediksi dengan KNN
+  prediksi = predictknn(data_uji, X, y, 4)
+    
+  # Mengolah hasil Prediksi
+  hasil = ""
+  if(prediksi == 1):
+    hasil = "Normal"
+  elif(prediksi == 2):
+    hasil = "Suspect"
+  else:
+    hasil = "Pathological"
+
+  return hasil
+
+#END DIAGNOSIS
+
+# PREDIKSI KNN
+
+def predictknn(point, data, label, n):
+  distances = []
+  for i in range(len(data)):
+    distances.append([euclidean(point, data[i]), label[i]])
+  distances.sort(key=lambda x: x[0])
+  top_classes = [x[1] for x in distances[0:n]]
+  repeat = True
+  try:
+    y_hat = mode(top_classes)
+  except:
+    while repeat:
+      try:
+        top_classes = top_classes[:-1]
+        y_hat = mode(top_classes)
+        repeat = False
+      except:
+        pass
+  return y_hat
+
+#END PREDIKSI KNN
+
+#NORMALISASI MINMAX
+
+def MinMaxScalerManual(fit,b):
+  minmax = getMinMax(fit)
+  hasil = []
+  for i in range(len(b)):
+    baris = []
+    for j in  range(len(b[i])):
+      pembagi = minmax[j][1]-minmax[j][0]
+      if (pembagi == 0):
+        pembagi = 1
+      c = (b[i][j] - minmax[j][0])/pembagi
+      baris.append(c)
+    hasil.append(baris)
+  return hasil
+
+#END NORMALISASI MANUAL
+
+# GET NILAI MIN AND MAX
+
+def getMinMax(x):
+  minmax = []
+  for i in range(0,21):
+    kolom =[]
+    min = x[0][i]
+    max = x[0][i]
+    for j in range(len(x)):
+      if min > x[j][i]:
+        min = x[j][i]
+      if max < x[j][i]:
+        max = x[j][i]
+    kolom.append(min)
+    kolom.append(max)
+    minmax.append(kolom)
+  return minmax
+
+#END GET MINMAX
+
+#PENGELOMPOKKAN PER LABEL / KELAS
+
+#LABEL 1 / LABEL NORMAL
 def label1(data):
   panjang = len(data)
   a = []
@@ -56,6 +195,8 @@ def label1(data):
     if(data[i][0] == 1):
       a.append(data[i][0:])
   return a
+
+#LABEL 2 / LABEL SUSPECT
 def label2(data):
   panjang = len(data)
   a = []
@@ -63,6 +204,8 @@ def label2(data):
     if(data[i][0] == 2):
       a.append(data[i][0:])
   return a
+
+#LABEL 3 / LABEL PATHOLOGIC
 def label3(data):
   panjang = len(data)
   a = []
@@ -70,6 +213,10 @@ def label3(data):
     if(data[i][0] == 3):
       a.append(data[i][0:])
   return a
+
+#END PENGELOMPOKKAN DATA PER LABEL
+
+#K-FOLD CROSS VALIDATION
 
 def kfcv(dataA, dataB, dataC, kfold, k):
   KA = int(len(dataA)/kfold)
@@ -133,6 +280,10 @@ def kfcv(dataA, dataB, dataC, kfold, k):
 
   return hasilFold
 
+#END K-FOLD CROSS VALIDATION
+
+#PENGUJIAN 1 UNTUK MENDAPATKAN NILAI K TERBAIK
+
 def pengujian1(kelas1,kelas2,kelas3):
     akurasi = []
     kterbaik = 0
@@ -140,53 +291,261 @@ def pengujian1(kelas1,kelas2,kelas3):
     terbaik = []
     hasil = []
 
-    for i in range(3,23):
-    # for i in range(3,7):
-      start_time = time.time()
-      a = []
-      a.append(i)
-      nilai = np.array(kfcv(kelas1,kelas2,kelas3,10, i)).mean()
-      a.append(round(nilai, 3))
-      end_time = time.time()
-      time_lapsed = end_time - start_time
-      time_lapsed = time_convert(time_lapsed)
-      print(f"K: {a[0]} --> {a[1]}  ; time = {time_lapsed}")
-      a.append(time_lapsed)
-      akurasi.append(a)
-      if(nilaikterbaik < a[1]):
-        nilaikterbaik = a[1]
-        kterbaik = a[0]
+    for i in range(3,35):
+      if(i%2 == 0):
+        start_time = time.time()
+        a = []
+        a.append(i)
+        nilai = np.array(kfcv(kelas1,kelas2,kelas3,10, i)).mean()
+        a.append(round(nilai, 4))
+        end_time = time.time()
+        time_lapsed = end_time - start_time
+        time_lapsed = time_convert(time_lapsed)
+        print(f"K: {a[0]} --> {a[1]}  ; time = {time_lapsed}")
+        a.append(time_lapsed)
+        akurasi.append(a)
+        if(nilaikterbaik < a[1]):
+          nilaikterbaik = a[1]
+          kterbaik = a[0]
     terbaik.append(kterbaik)
-    terbaik.append(round(nilaikterbaik, 3))
+    terbaik.append(round(nilaikterbaik, 4))
     hasil.append(terbaik)
     hasil.append(akurasi)
     return hasil
 
-def pengujian2(kelas1,kelas2,kelas3):
-    akurasi = []
-    foldterbaik = 0
-    nilaifoldterbaik = 0
-    terbaik = []
-    hasil = []
+#END PENGUJIAN 1
 
-    for i in range(2,11):
-    # for i in range(2,5):
-      start_time = time.time()
-      a = []
-      a.append(i)
-      nilai = np.array(kfcv(kelas1,kelas2,kelas3,i, 4)).mean()
-      a.append(round(nilai, 3))
-      end_time = time.time()
-      time_lapsed = end_time - start_time
-      time_lapsed = time_convert(time_lapsed)
-      print(f"Kfold: {a[0]} --> {a[1]}  ; time = {time_lapsed}")
-      a.append(time_lapsed)
-      akurasi.append(a)
-      if(nilaifoldterbaik < a[1]):
-        nilaifoldterbaik = a[1]
-        foldterbaik = a[0]
-    terbaik.append(foldterbaik)
-    terbaik.append(round(nilaifoldterbaik, 3))
-    hasil.append(terbaik)
-    hasil.append(akurasi)
-    return hasil
+#PENGUJIAN 2 UNTUK MENDAPATKAN FOLD TERBAIK DAN DATA LATIH TERBAIK
+
+def pengujian2(dataA, dataB, dataC, Ka, nilaiK):
+  KA = int(len(dataA)/Ka)
+  KB = int(len(dataB)/Ka)
+  KC = int(len(dataC)/Ka)
+  KA0 = 0
+  KB0 = 0
+  KC0 = 0
+  hasil_fold = []
+  fold_terbaik = []
+  nilai_fold = 0
+  # data_train_terbaik = []
+  # data_test_terbaik = []
+  hasil_pengujian = []
+  terbaik = []
+  
+
+  for i in range(Ka):
+    a = []
+    start_time = time.time()
+    klsTest = []
+    klsTrain = []
+    train = []
+    test = []
+    data_train = []
+    data_test = []
+    # dataNormalTrain = 0
+    # dataNormalTest = 0
+    # dataSuspectTrain = 0
+    # dataSuspectTest = 0
+    # dataPathologicTrain = 0
+    # dataPathologicTest = 0
+
+    if i < 5 :
+      if i == 0:
+        KA0 = 0
+        KB0 = 0
+        KC0 = 0
+        KAp = 165
+        KBp = 166
+        KCp = 166
+      else:
+        KA0 = KAp
+        KB0 = KBp
+        KC0 = KCp
+        KAp += 165
+        KBp += 166
+        KCp += 166
+    else :
+      KA0 = KAp
+      KB0 = KBp
+      KC0 = KCp
+      KAp += 166
+      KBp += 165
+      KCp += 165
+
+    for j in range(len(dataA)):
+      if j>=KA0 and j<KAp :
+        data_test.append(dataA[j][0:])
+        # dataNormalTest += 1
+      else:
+        data_train.append(dataA[j][0:])
+        # dataNormalTrain += 1
+
+    for j in range(len(dataB)):
+      if j>=KB0 and j<KBp :
+        data_test.append(dataB[j][0:])
+        # dataSuspectTest += 1
+      else:
+        data_train.append(dataB[j][0:])
+        # dataSuspectTrain += 1
+
+    for j in range(len(dataC)):
+      if j>=KC0 and j<KCp :
+        data_test.append(dataC[j][0:])
+        # dataPathologicTest += 1
+      else:
+        data_train.append(dataC[j][0:])
+        # dataPathologicTrain += 1
+
+    for j in range(len(data_train)):
+      klsTrain.append(data_train[j][0])
+    train = np.delete(data_train, [0,0], axis=1)
+
+    for j in range(len(data_test)):
+      klsTest.append(data_test[j][0])
+    test = np.delete(data_test, [0,0], axis=1)
+
+    a.append(i+1)
+    #KNN
+    akurasi = knn(train, klsTrain, test, klsTest, nilaiK)
+
+    end_time = time.time()
+    time_lapsed = end_time - start_time
+    time_lapsed = time_convert(time_lapsed)
+    a.append(akurasi)
+    a.append(time_lapsed)
+
+    hasil_fold.append(a)
+
+    if nilai_fold < akurasi :
+      fold_terbaik = i+1
+      nilai_fold = akurasi
+      # data_train_terbaik = data_train
+      # data_test_terbaik = data_test
+    print(f"Fold : {i+1}; Akurasi: {akurasi}; time: {time_lapsed}")
+
+  terbaik.append(fold_terbaik)
+  terbaik.append(nilai_fold)
+
+  # print(f"Fold Terbaik: {fold_terbaik}; Akurasi: {nilai_fold}")
+  hasil_pengujian.append(terbaik)
+  hasil_pengujian.append(hasil_fold)
+  
+  return hasil_pengujian
+
+# def pengujian2(kelas1,kelas2,kelas3):
+#     akurasi = []
+#     foldterbaik = 0
+#     nilaifoldterbaik = 0
+#     terbaik = []
+#     hasil = []
+
+#     for i in range(2,11):
+#     # for i in range(2,5):
+#       start_time = time.time()
+#       a = []
+#       a.append(i)
+#       nilai = np.array(kfcv(kelas1,kelas2,kelas3,i, 4)).mean()
+#       a.append(round(nilai, 4))
+#       end_time = time.time()
+#       time_lapsed = end_time - start_time
+#       time_lapsed = time_convert(time_lapsed)
+#       print(f"Kfold: {a[0]} --> {a[1]}  ; time = {time_lapsed}")
+#       a.append(time_lapsed)
+#       akurasi.append(a)
+#       if(nilaifoldterbaik < a[1]):
+#         nilaifoldterbaik = a[1]
+#         foldterbaik = a[0]
+#     terbaik.append(foldterbaik)
+#     terbaik.append(round(nilaifoldterbaik, 4))
+#     hasil.append(terbaik)
+#     hasil.append(akurasi)
+#     return hasil
+
+#END PENGUJIAN 2
+
+
+#GET DATA LATIH FOLD TERBAIK
+def getDataLatih(dataA, dataB, dataC, Ka):
+  KA0 = 0
+  KB0 = 0
+  KC0 = 0
+
+  for i in range(Ka):
+    if i < 5 :
+      if i == 0:
+        KA0 = 0
+        KB0 = 0
+        KC0 = 0
+        KAp = 165
+        KBp = 166
+        KCp = 166
+      else:
+        KA0 = KAp
+        KB0 = KBp
+        KC0 = KCp
+        KAp += 165
+        KBp += 166
+        KCp += 166
+    else :
+      KA0 = KAp
+      KB0 = KBp
+      KC0 = KCp
+      KAp += 166
+      KBp += 165
+      KCp += 165
+
+  #endfor
+
+  data_train = []
+  data_test = []
+  dataNormalTrain = 0
+  dataNormalTest = 0
+  dataSuspectTrain = 0
+  dataSuspectTest = 0
+  dataPathologicTrain = 0
+  dataPathologicTest = 0
+  klsTrain = []
+  klsTest = []
+  
+  for j in range(len(dataA)):
+    if j>=KA0 and j<KAp :
+      data_test.append(dataA[j][0:])
+      dataNormalTest += 1
+    else:
+      data_train.append(dataA[j][0:])
+      dataNormalTrain += 1
+
+  for j in range(len(dataB)):
+    if j>=KB0 and j<KBp :
+      data_test.append(dataB[j][0:])
+      dataSuspectTest += 1
+    else:
+      data_train.append(dataB[j][0:])
+      dataSuspectTrain += 1
+
+  for j in range(len(dataC)):
+    if j>=KC0 and j<KCp :
+      data_test.append(dataC[j][0:])
+      dataPathologicTest += 1
+    else:
+      data_train.append(dataC[j][0:])
+      dataPathologicTrain += 1
+
+  for j in range(len(data_train)):
+    # print(f"data Train: {j}; ")
+    klsTrain.append(data_train[j][0])
+    train = np.delete(data_train, [0,0], axis=1)
+
+  # for j in range(len(data_test)):
+  #     # print(f"datatest: {j}; ")
+  #     klsTest.append(data_test[j][0])
+  #     test = np.delete(data_test, [0,0], axis=1)
+
+  # print(f"Fold: {i+1}; Data Latih: {len(data_train)} dan Data Uji: {len(data_test)}")
+  # print(f"DataNormalTrain: {dataNormalTrain}; DataNormalTest: {dataNormalTest}")
+  # print(f"DataSuspectTrain: {dataSuspectTrain}; DataSuspectTest: {dataSuspectTest}")
+  # print(f"DataPathologicTrain: {dataPathologicTrain}; DataPathologicTest: {dataPathologicTest}")
+  # print(f" ")
+
+
+  return data_train,train,klsTrain
